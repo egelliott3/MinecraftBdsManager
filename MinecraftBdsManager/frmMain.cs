d7012796b5f8b749e7b2ec8449d5da8f8a4d9515
Earl Elliott <eelliott@microsoft.com>
2021-12-22 15:32:16 -0800

    Initial main form layout

---
+++ MinecraftBdsManager/frmMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinecraftBdsManager
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIssueCommand_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolBtnBackupNow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolBtnOpenLogsFolder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolBtnOpenSavesFolder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolBtnShowMap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolBtnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolBtnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolBtnViewLog_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
